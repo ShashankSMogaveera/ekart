20302cf9cefbd818b79daa75b57020b518b598f7
shashank-epam <shashankshekar_mogaveera@epam.com>
2024-11-22 13:48:32 +0530

    chores: create basic application ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent} from '../app/products/products.component'
+import { SearchComponent } from '../app/searchbox/search-box.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, ProductsComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ekart';
+  count=0;
+
+  incrementCount(){
+    this.count++;
+  }
+  decrementCount(){
+    this.count--;
+  }
 }
